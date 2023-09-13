e8d9bca811fdedd12b05bcca6cb8c37d4fcf2242
TJBaumgartner <tnorth6@gmail.com>
2023-09-12 21:37:02 -0700

    created cases for contact and about url + error

--- index.js
+++ index.js
@@ -4,15 +4,52 @@ const port = 8080;
 
 const server = http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type' : 'text/html' });
-    fs.readFile('pages/index.html', function(error, data) {
-        if(error){
-            response.writeHead(404);
-            response.write('Error: File Not Found');
-        }else{
-            response.write(data)
-        }
-        response.end();
-    })
+    switch(request.url){
+        case "/":
+            fs.readFile('pages/index.html', function(error, data) {
+                if(error){
+                    response.writeHead(404);
+                    response.write('Error: File Not Found');
+                }else{
+                    response.write(data)
+                }
+                response.end();
+            })
+        break
+        case "/about":
+            fs.readFile('pages/about.html', function(error, data) {
+                if(error){
+                    response.writeHead(404);
+                    response.write('Error: File Not Found');
+                }else{
+                    response.write(data)
+                }
+                response.end();
+            })
+        break
+        case "/contact":
+            fs.readFile('pages/contact.html', function(error, data) {
+                if(error){
+                    response.writeHead(404);
+                    response.write('Error: File Not Found');
+                }else{
+                    response.write(data)
+                }
+                response.end();
+            })
+        break
+        default:
+            fs.readFile('pages/error.html', function(error, data) {
+                if(error){
+                    response.writeHead(404);
+                    response.write('Error: File Not Found');
+                }else{
+                    response.write(data)
+                }
+                response.end();
+            })
+        break
+    }
 });
 server.listen(port, function(error) {
     if(error){
